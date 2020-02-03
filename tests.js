93c00608c59e0af6068abc1d6cd635a12e3fa751
Tripple-A <biodun9@gmail.com>
2020-02-04 00:08:54 +0100

    added the calculator module and it's tests

--- tests.js
+++ tests.js
@@ -2,4 +2,22 @@ const capitalize = (string) => string[0].toUpperCase() + string.slice(1);
 
 const reverse = (string) => string.split('').reverse().join('');
 
-export { capitalize, reverse };
+const Calculator = (() => {
+  const add = (a, b) => {
+    return a + b;
+  };
+  const subtract = (a, b) => {
+    return a - b;
+  };
+  const divide = (a, b) => {
+    return a / b;
+  };
+  const multiply = (a, b) => {
+    return a * b;
+  };
+  return {
+    add, subtract, divide, multiply,
+  };
+})();
+
+export { capitalize, reverse, Calculator };
--- tests.test.js
+++ tests.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverse } from './tests';
+import { capitalize, reverse, Calculator } from './tests';
 
 test('it takes in a string and returns same with first letter capitalized', () => {
   expect(capitalize('girl')).toBe('Girl');
@@ -6,4 +6,12 @@ test('it takes in a string and returns same with first letter capitalized', () =
 
 test('it takes in a string and returns the reverse of the string', () => {
   expect(reverse('girl')).toBe('lrig');
+});
+
+test('it takes executes basic calculator functions', () => {
+  expect(Calculator.add(1, 2)).toBe(3);
+  expect(Calculator.subtract(9, 2)).toBe(7);
+  expect(Calculator.divide(9, 2)).toBeCloseTo(4.5);
+  expect(Calculator.divide('s', 'l')).toBeNaN();
+  expect(Calculator.multiply(10, 5)).toBe(50);
 });
