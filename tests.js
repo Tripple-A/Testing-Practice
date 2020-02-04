d07001f3bf6185b82c8dda98c051921b2181a5a3
Tripple-A <biodun9@gmail.com>
2020-02-04 10:01:33 +0100

    added caesar cipher functions

--- tests.js
+++ tests.js
@@ -3,21 +3,39 @@ const capitalize = (string) => string[0].toUpperCase() + string.slice(1);
 const reverse = (string) => string.split('').reverse().join('');
 
 const Calculator = (() => {
-  const add = (a, b) => {
-    return a + b;
-  };
-  const subtract = (a, b) => {
-    return a - b;
-  };
-  const divide = (a, b) => {
-    return a / b;
-  };
-  const multiply = (a, b) => {
-    return a * b;
-  };
+  const add = (a, b) => (a + b);
+
+  const subtract = (a, b) => a - b;
+
+  const divide = (a, b) => a / b;
+
+  const multiply = (a, b) => a * b;
+
   return {
     add, subtract, divide, multiply,
   };
 })();
 
-export { capitalize, reverse, Calculator };
+const caesar = (str, k) => {
+  const check1 = (newcode) => String.fromCharCode(((newcode - 122) % 26) + 96);
+
+  const check2 = (newcode) => String.fromCharCode(newcode);
+
+  const check3 = (newcode) => String.fromCharCode(((newcode - 90) % 26) + 64);
+
+  for (let index=0; index < str.length; index++) {
+    const letter = str.charAt(index);
+    const code = str.charCodeAt(index);
+    const newcode = code + k;
+    if (code >= 97 && code <= 122) {
+      newcode > 122 ? str = str.replace(letter, check1(newcode)) : str = str.replace(letter, check2(newcode));
+    }
+    if (code >= 65 && code <= 90) {
+      newcode > 90 ? str = str.replace(letter, check3(newcode)) : str = str.replace(letter, check2(newcode));
+    }
+  }
+  return str;
+};
+export {
+  capitalize, reverse, Calculator, caesar,
+};
--- tests.test.js
+++ tests.test.js
@@ -1,4 +1,6 @@
-import { capitalize, reverse, Calculator } from './tests';
+import { 
+  capitalize, reverse, Calculator, caesar, analyze,
+} from './tests';
 
 test('it takes in a string and returns same with first letter capitalized', () => {
   expect(capitalize('girl')).toBe('Girl');
@@ -14,4 +16,19 @@ test('it takes executes basic calculator functions', () => {
   expect(Calculator.divide(9, 2)).toBeCloseTo(4.5);
   expect(Calculator.divide('s', 'l')).toBeNaN();
   expect(Calculator.multiply(10, 5)).toBe(50);
+});
+
+test('it takes in a string and returns the caesar cipher of the string', () => {
+  expect(caesar('abc', 3)).toBe('def');
+  expect(caesar('xyz', 5)).toBe('cde');
+  expect(caesar('middle-Outz', 2)).toBe('okffng-Qwvb');
+});
+
+test('it takes in an array and returns an object with min,max,average and length values', () => {
+  expect(analyze([1, 8, 3, 4, 2, 6])).toEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
